4998efaa304a7aaba5de4aed9db5965cd8c7eff1
Howie Z <masterxdh20@gmail.com>
2018-07-22 21:55:16 -0400

    created game logic

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log("Up and running!");
+var cardOne = "King";
+var cardTwo = "Queen";
+var cardThree = "King";
+var cardFour = "Queen";
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
+console.log("User flipped " + cardThree);
+console.log("User flipped " + cardFour);
